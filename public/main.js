bbb51681c895ec08386cfbef6a86cb5440f5980d
osyoyu <osyoyu@osyoyu.com>
2016-01-28 10:47:35 +0900

    Prevent registering onClick() event on every WebSocket reconnect

--- public/main.js
+++ public/main.js
@@ -41,10 +41,10 @@
   function onSignal (signal) {
     switch (signal.type) {
     case 'counters':
-      construct(signal.counters);
+      // construct(signal.counters);
       break;
     case 'changed':
-      counters[signal.counter_name].update(signal.counter_value);
+      counters['saikoh'].update(signal.counter_value);
       break;
     }
   }
@@ -65,37 +65,36 @@
     });
   }
 
-  function construct (receivedCounters) {
-    var $counters = receivedCounters.map(function (counter) {
-      var $value = T(counter.counter_value.toString());
-      var $btn = document.getElementById('btn-saikoh');
-      counters[counter.counter_name] = {
-        play: function () {
-          var source = audioContext.createBufferSource();
-          source.connect(audioContext.destination);
-          source.buffer = this.buffer;
-          source.start();
-        },
-        update: function (value) {
-          $value.nodeValue = value;
-        }
-      };
+  // var $value = T(counter.counter_value.toString());
+  var $btn = document.getElementById('btn-saikoh');
+  counters['saikoh'] = {
+    play: function () {
+      var source = audioContext.createBufferSource();
+      source.connect(audioContext.destination);
+      source.buffer = this.buffer;
+      source.start();
+    },
+    update: function (value) {
+      // $value.nodeValue = value;
+    }
+  };
 
-      fetchAudio("/audio/" + counter.counter_name + ".mp3").then(function (data) {
-        audioContext.decodeAudioData(data, function (buffer) {
-          counters[counter.counter_name].buffer = buffer;
-        });
-        $btn.removeAttribute('disabled');
-      });
+  fetchAudio("/audio/saikoh.mp3").then(function (data) {
+    audioContext.decodeAudioData(data, function (buffer) {
+      counters['saikoh'].buffer = buffer;
+    });
+    $btn.removeAttribute('disabled');
+  });
 
-      function onClick (e) {
-        cmd.countup(counter.counter_name);
-        counters[counter.counter_name].play();
-        window.navigator.vibrate(100);
-      }
-
-      $btn.addEventListener('touchstart', function (e) { e.stopPropagation(); e.preventDefault(); onClick(); } , false);
-      $btn.addEventListener('click', onClick, false);
-    }, {});
+  function onClick (e) {
+    cmd.countup('saikoh');
+    counters['saikoh'].play();
+    window.navigator.vibrate(100);
   }
+
+  $btn.addEventListener('touchstart', function (e) {
+    e.stopPropagation(); e.preventDefault(); onClick();
+  }, false);
+  $btn.addEventListener('click', onClick, false);
+
 })();
